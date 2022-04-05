25ddf02b81803f172da6df30eee14d3d7e2bbbe6
mrobe1 <michael.roberts@cloudreach.com>
2022-04-05 10:57:15 -0400

    Format code

--- variable.tf
+++ variable.tf
@@ -1,30 +1,30 @@
 # VPC Variables
 variable "region" {
-  default       = "us-east-1"
-  description   = "AWS Region"
-  type          = string
+  default     = "us-east-1"
+  description = "AWS Region"
+  type        = string
 }
 
 variable "vpc-cidr" {
-  default       = "192.168.0.0/16"
-  description   = "VPC CIDR Block"
-  type          = string
+  default     = "192.168.0.0/16"
+  description = "VPC CIDR Block"
+  type        = string
 }
 
 variable "public-subnet-1-cidr" {
-  default       = "192.168.1.0/24"
-  description   = "Public Subnet 1 CIDR Block"
-  type          = string
+  default     = "192.168.1.0/24"
+  description = "Public Subnet 1 CIDR Block"
+  type        = string
 }
 
 variable "private-subnet-1-cidr" {
-  default       = "192.168.2.0/24"
-  description   = "Private Subnet 1 CIDR Block"
-  type          = string
+  default     = "192.168.2.0/24"
+  description = "Private Subnet 1 CIDR Block"
+  type        = string
 }
 
 variable "private-subnet-2-cidr" {
-  default       = "192.168.3.0/24"
-  description   = "Private Subnet 2 CIDR Block"
-  type          = string
+  default     = "192.168.3.0/24"
+  description = "Private Subnet 2 CIDR Block"
+  type        = string
 }
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "main" {
-  cidr_block = "${var.vpc-cidr}"
+  cidr_block = var.vpc-cidr
 
   tags = {
-      Name = "ta-mikes-vpc"
+    Name = "ta-mikes-vpc"
   }
 }
 
@@ -15,13 +15,13 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_nat_gateway" "my_nat_gateway" {
-    allocation_id = aws_eip.nat_eip.id
-    subnet_id     = aws_subnet.public-subnet-1.id 
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public-subnet-1.id
 
-    tags = {
-        Name = "nat gate pub"
-    }
-    depends_on = [aws_internet_gateway.igw]
+  tags = {
+    Name = "nat gate pub"
+  }
+  depends_on = [aws_internet_gateway.igw]
 }
 
 resource "aws_eip" "nat_eip" {
@@ -31,86 +31,86 @@ resource "aws_eip" "nat_eip" {
 
 #Public Subnet
 resource "aws_subnet" "public-subnet-1" {
-    vpc_id                  = aws_vpc.main.id
-    cidr_block              = "${var.public-subnet-1-cidr}"
-    availability_zone       = "us-east-1a"
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public-subnet-1-cidr
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
-    tags = {
-        Name = "public-subnet-1"
-    }
+  tags = {
+    Name = "public-subnet-1"
+  }
 }
 
 # Create Route Table and Add Public Route
 # terraform aws create route table
 resource "aws_route_table" "public-route-table" {
-  vpc_id       = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
-  tags       = {
-    Name     = "Public Route Table"
+  tags = {
+    Name = "Public Route Table"
   }
 }
 
 resource "aws_route_table" "private-route-table" {
-  vpc_id       = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.my_nat_gateway.id
   }
 
-  tags       = {
-    Name     = "Private Route Table"
+  tags = {
+    Name = "Private Route Table"
   }
 }
 
 # Associate Public Subnet 1 to "Public Route Table"
 # terraform aws associate subnet with route table
 resource "aws_route_table_association" "public-subnet-1-route-table-association" {
-  subnet_id           = aws_subnet.public-subnet-1.id
-  route_table_id      = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-route-table.id
 }
 
 # Associate Private Subnet 1 to "App Layer"
 resource "aws_route_table_association" "private-subnet-1-route-table-association-App" {
-  subnet_id           = aws_subnet.private-subnet-1.id
-  route_table_id      = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-1.id
+  route_table_id = aws_route_table.private-route-table.id
 }
 
 # Associate Private Subnet 2 to "App Layer"
 resource "aws_route_table_association" "private-subnet-2-route-table-association-App" {
-  subnet_id           = aws_subnet.private-subnet-2.id
-  route_table_id      = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-2.id
+  route_table_id = aws_route_table.private-route-table.id
 }
 
 
 
 #Private Subnets
 resource "aws_subnet" "private-subnet-1" {
-    vpc_id                  = aws_vpc.main.id
-    cidr_block              = "${var.private-subnet-1-cidr}"
-    availability_zone       = "us-east-1a"
-    map_public_ip_on_launch = false
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private-subnet-1-cidr
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
 
-    tags = {
-        Name = "Private Subnet 1 | App layer"
-    }
+  tags = {
+    Name = "Private Subnet 1 | App layer"
+  }
 }
 
 resource "aws_subnet" "private-subnet-2" {
-    vpc_id                  = aws_vpc.main.id
-    cidr_block              = "${var.private-subnet-2-cidr}"
-    availability_zone       = "us-east-1a"
-    map_public_ip_on_launch = false
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private-subnet-2-cidr
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
 
-    tags = {
-        Name = "Private Subnet 2 | Data layer"
-    }
+  tags = {
+    Name = "Private Subnet 2 | Data layer"
+  }
 }
 
 # resource "aws_dynamodb_table" "terraform_lock_tbl" {
